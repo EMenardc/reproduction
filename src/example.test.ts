8e5c4fb4fd791976568b9f93c546fadc48e42546
EMenardc <emenardc@gmail.com>
2024-10-31 12:23:53 -0400

    test

--- src/example.test.ts
+++ src/example.test.ts
@@ -1,7 +1,7 @@
-import { Entity, MikroORM, PrimaryKey, Property } from '@mikro-orm/sqlite';
+import { Collection, Entity, LoadStrategy, ManyToOne, MikroORM, OneToMany, PrimaryKey, PrimaryKeyProp, Property } from '@mikro-orm/mssql';
 
 @Entity()
-class User {
+class Client {
 
   @PrimaryKey()
   id!: number;
@@ -9,13 +9,44 @@ class User {
   @Property()
   name: string;
 
-  @Property({ unique: true })
-  email: string;
+
+  @OneToMany(() => EmployeeClients, employee => employee.client,  { strategy: LoadStrategy.JOINED } )
+  employeeClients = new Collection<EmployeeClients>(this);
+
 
   constructor(name: string, email: string) {
     this.name = name;
-    this.email = email;
   }
+
+}
+
+@Entity()
+class Employee {
+
+  @PrimaryKey()
+  id!: number;
+
+  @Property()
+  name: string;
+
+  constructor(name: string, email: string) {
+    this.name = name;
+  }
+
+}
+
+@Entity()
+class EmployeeClients {
+
+  [PrimaryKeyProp]?: ['client', 'employee'];
+  
+  // FIELD NAME DOES NOT WORK ON DELETE AND UPDATES MANY TO ONE
+  @ManyToOne({ entity: () => Client, primary: true, fieldName: 'client_id' })
+  client!: Client;
+
+  // FIELD NAME DOES NOT WORK ON DELETE AND UPDATES MANY TO ONE
+  @ManyToOne({ entity: () => Employee, primary: true,  fieldName: 'employee_id', strategy: LoadStrategy.JOINED  })
+  employee!: Employee;
 
 }
 
@@ -23,29 +54,56 @@ let orm: MikroORM;
 
 beforeAll(async () => {
   orm = await MikroORM.init({
-    dbName: ':memory:',
-    entities: [User],
-    debug: ['query', 'query-params'],
-    allowGlobalContext: true, // only for testing
+    dbName: "reproduction",
+    host: 'host',
+    user: 'user',
+    password: 'password',
+    entities: [Client, Employee, EmployeeClients],
+    debug: ["query", "query-params"],
+    allowGlobalContext: true
   });
   await orm.schema.refreshDatabase();
 });
 
 afterAll(async () => {
+  await orm.schema.dropSchema();
   await orm.close(true);
 });
 
 test('basic CRUD example', async () => {
-  orm.em.create(User, { name: 'Foo', email: 'foo' });
-  await orm.em.flush();
-  orm.em.clear();
+ await orm.em.create(Client,{
+    id: 1,
+    name: 'The client'
+  });
 
-  const user = await orm.em.findOneOrFail(User, { email: 'foo' });
-  expect(user.name).toBe('Foo');
-  user.name = 'Bar';
-  orm.em.remove(user);
-  await orm.em.flush();
+ await orm.em.create(Employee,{
+    id: 1,
+    name: 'The employee'
+  });
 
-  const count = await orm.em.count(User, { email: 'foo' });
-  expect(count).toBe(0);
+  await orm.em.create(EmployeeClients,{
+    employee: 1,
+    client: 1
+  });
+
+  orm.em.flush();
+
+  await orm.em.begin();
+
+  try {
+    let find = await orm.em.find(EmployeeClients, { client: 1, employee: 1 });
+
+      await orm.em.remove(find);
+      await orm.em.flush();
+      await orm.em.commit();
+  } catch(e) {
+      await orm.em.rollback();
+      throw e;
+  }
+ 
+
+
+
+
+
 });
